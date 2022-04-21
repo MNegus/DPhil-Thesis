0b676466816f838b692b214f3655f33e14fd4444
Michael <michaelnegus1@gmail.com>
2022-04-21 09:26:38 +0100

    Comparisons between numerical computations and analytical

--- Figure_Scripts/PressureForceComparison.m
+++ Figure_Scripts/PressureForceComparison.m
@@ -20,13 +20,23 @@ J = @(t) pi * d(t) ./ (8 * d_t(t).^2);
 A = @(t) d(t) .* d_t(t);
 C = @(t) A(t);
 
-ts = linspace(1e-3, 10, 20);
+ts = linspace(1e-3, 50, 50);
+
+%% Solve for c0 function
+c0Guess = 0.5 * log(pi * d(ts) ./ (epsilon^2 * J(ts)));
+zero_fun = @(c) exp(2 * c) + 4 * exp(c) + 2 * c + 1 - pi * d(ts) ./ (epsilon^2 * J(ts));
+c0s = fsolve(zero_fun, c0Guess);
+% c0s = c0Guess
 
 %% Forces values
 FouterExact = pi * A(ts);
-FcompExact = pi * A(ts) + 8 * sqrt(J(ts) / pi) .* d(ts).^(5/2) - 2 * sqrt(2) * A(ts);
+% FinnerExact = 8 * sqrt(J(ts) / pi) .* d(ts).^(5/2
+% FinnerExact = 8 * epsilon * d_t(ts).^2 .* J(ts) .* exp(c0s) / pi;
+FinnerExact = 8 * d_t(ts).^2 .* sqrt(d(ts) .* J(ts)) / pi;
+FcompExact = FouterExact + FinnerExact - 2 * sqrt(2) * A(ts);
 
 FouterNumerical = zeros(size(ts));
+FinnerNumerical = zeros(size(ts));
 FcompNumerical = zeros(size(ts));
 
 %% Plots in time
@@ -35,8 +45,8 @@ for k = 1 : length(ts)
     t = ts(k)
 
     %% Inner pressure
-    c0 = log(sqrt(pi * d(t) ./ (epsilon^2 * J(t))));
-    sigmas = linspace(-c0, 1e3, 1e5);
+    
+    sigmas = linspace(-c0s(k), 1e3, 1e5);
     xTildes = (J(t) / pi) * (2 * sigmas - exp(-2 * sigmas) - 4 * exp(-sigmas) - 1);
     xs = epsilon * d(t) + epsilon^3 * xTildes;
 
@@ -45,7 +55,6 @@ for k = 1 : length(ts)
     %% Outer and overlap pressure
     xHats = xs / epsilon;
     xHats = xHats(xHats < d(t));
-    size(xHats)
     psOuter = A(t) ./ (epsilon * sqrt(d(t).^2 - xHats.^2));
     psOverlap = C(t) ./ (epsilon * sqrt(2 * d(t)) * sqrt(d(t) - xHats));
     
@@ -54,17 +63,22 @@ for k = 1 : length(ts)
     psComp(xHats < d(t)) = psComp(xHats < d(t)) + psOuter - psOverlap;
 
     %% Numerical calculation
-    FouterNumerical(k) = 2 * trapz(epsilon * xHats, psOuter);
+    
+    FouterNumerical(k) = 2 * trapz(xHats, epsilon * psOuter);
+    FinnerNumerical(k) = 2 * trapz(xs, psInner);
+    
     FcompNumerical(k) = 2 * trapz(xs, psComp);
     
     %% Plots
     plot(epsilon * xHats, psOuter);
     hold on;
     plot(xs, psInner);
-    plot(xs, psComp);
+    plot(xs, psComp, 'Color', 'Black', 'linewidth', 1);
     hold off;
+    
+    xlim([-0.01, 0.2]);
+    ylim([0, 700]);
     drawnow;
-    ylim([0, 1.5 * max(psComp)]);
     pause(0.1);
 
 end
@@ -73,8 +87,14 @@ end
 close(figure(2));
 figure(2);
 hold on;
-plot(ts, FouterNumerical, 'Color', 'Black', 'Linewidth', 1);
-plot(ts, FouterExact, 'Color', 'Black', 'Linewidth', 1, 'Linestyle', '--');
+plot(ts, FouterNumerical, 'Color', 'Black', 'Linewidth', 2, 'Displayname', 'Numerical Outer');
+plot(ts, FouterExact, 'Color', 'Black', 'Linewidth', 2, 'Linestyle', '--', 'Displayname', 'Exact Outer');
+
+plot(ts, FinnerNumerical, 'Color', 'Blue', 'Linewidth', 2, 'Displayname', 'Numerical Inner');
+plot(ts, FinnerExact, 'Color', 'Blue', 'Linewidth', 2, 'Linestyle', '--', 'Displayname', 'Exact Inner');
+
 plot(ts, FcompNumerical, 'Color', 0.5 * [1 1 1], 'Linewidth', 1);
-% plot(ts, FcompExact, 'Color', 0.5 * [1 1 1], 'Linewidth', 2, 'Linestyle', '--');
+plot(ts, FcompExact, 'Color', 0.5 * [1 1 1], 'Linewidth', 2, 'Linestyle', '--', 'Displayname', 'Composite');
+legend();
+% ylim([5, 10]);
 
