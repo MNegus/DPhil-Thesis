f22740c80138357e4c333ad6fae952544cf63f83
Michael <michaelnegus1@gmail.com>
2022-06-20 19:21:50 +0100

    Changing to only plot one timestep, as the evolution is covered in another script now

--- Analytical_Scripts/Polynomial/Figures/CompositePressure2D.m
+++ Analytical_Scripts/Polynomial/Figures/CompositePressure2D.m
@@ -14,11 +14,17 @@ set(0,'defaultAxesFontSize', 18);
 set(0, 'defaultAxesTickLabelInterpreter', 'latex');
 set(groot, 'DefaultLegendInterpreter', 'latex');
 
+%% Load in color map
+mapObj = load("fine_red_blue_cmap.mat");
+cmap = mapObj.cmap;
+blueCol = cmap(1, :);
+redCol = cmap(end, :);
+
 %% Parameters
-xs = linspace(0, 0.275, 5e3);
+xs = linspace(0, 0.4, 5e3);
 epsilon = 0.1;
 
-ts = [0.0625, 1.0];
+ts = [2];
 
 %% Substrate definition
 % Oscillatory time dependence
@@ -39,8 +45,7 @@ b_tt = @(t) - a_tt(t) / L^2;
 figure(1);
 hold on;
 
-% for tIdx = 1 : length(ts)
-for tIdx = 2
+for tIdx = 1 : length(ts)
     t = ts(tIdx);
     
     %% Save structure arrays
@@ -60,39 +65,26 @@ for tIdx = 2
     % Restrict outer solution where zero
     Stationary_ps_outer(Stationary_ps_outer == 0) = nan;
     
-    %% Find quadratic substrate pressures
-    [ps_composite, ps_outer, ps_inner] ...
-        = substratepressure(xs, SubstrateCoefficients, TimeDependents, epsilon);
-    
-    % Restrict outer solution where zero
-    ps_outer(ps_outer == 0) = nan;
-    
     %% Set line colour
-%     if tIdx == 2
-%         lineColor = 0.5 * [1 1 1];
-%     else
+
     StatlineColor = 'Black';
     lineColor = 0.5 * [1 1 1];
-%     end
     
     %% Plot pressures
-    % Outer pressures
-    plot(xs, Stationary_ps_outer, 'Linewidth', 3, 'Color', StatlineColor, ...
-        'Linestyle', '--', 'Displayname', 'Outer');
-    plot(xs, ps_outer, 'Linewidth', 3, 'Color', lineColor, ...
-        'Linestyle', '--', 'Displayname', 'Outer');
-    
-    % Inner pressures
-    plot(xs, Stationary_ps_inner, 'Linewidth', 3, 'Color', StatlineColor, ...
-        'Linestyle', ':', 'Displayname', 'Inner');
-    plot(xs, ps_inner, 'Linewidth', 3, 'Color', lineColor, ...
-        'Linestyle', ':', 'Displayname', 'Inner');
     
     % Composite pressures
-    plot(xs, Stationary_ps_composite, 'Linewidth', 1.85, 'Color', StatlineColor, ...
-            'Displayname', 'Composite');
-    plot(xs, ps_composite, 'Linewidth', 1.85, 'Color', lineColor, ...
-        'Displayname', 'Composite');
+    h(3) = plot(xs, Stationary_ps_composite, 'Linewidth', 2, 'Color', StatlineColor, ...
+            'Displayname', 'Composite solution');
+        
+    % Outer pressures
+    h(1) = plot(xs, Stationary_ps_outer, 'Linewidth', 3, 'Color', redCol, ...
+        'Linestyle', '--', 'Displayname', 'Outer solution');
+    
+    % Inner pressures
+    h(2) = plot(xs, Stationary_ps_inner, 'Linewidth', 3, 'Color', blueCol, ...
+        'Linestyle', ':', 'Displayname', 'Inner solution');
+    
+    
 %     
     %% Plot pressures
 %     h(3 * (tIdx - 1) + 1) = plot(xs, Stationary_ps_outer, 'Linewidth', 3, 'Color', lineColor, ...
@@ -103,21 +95,21 @@ for tIdx = 2
 %         'Displayname', 'Composite');
     
     %% Plot turnover line
-%     xline(epsilon * TimeDependents.ds, 'Linewidth', 0.5, 'Color', 0.5 * [1 1 1], ...
-%         'Linestyle', '--');
+    xline(epsilon * StationaryTimeDependents.ds, 'Linewidth', 0.5, 'Color', 0.5 * [1 1 1], ...
+        'Linestyle', '--');
     
 end
 
 %% Add annotations
-annotation('textbox',...
-    [0.14 0.5815 0.094 0.0675],...
-    'String',{'$t = 0.0625$'}, 'interpreter', 'latex', 'Fontsize', 18, ...
-    'Edgecolor', 'None');
-
-annotation('textbox',...
-    [0.65 0.5815 0.094 0.0675],...
-    'String',{'$t = 1$'}, 'interpreter', 'latex', 'Fontsize', 18, ...
-    'Edgecolor', 'None');
+% annotation('textbox',...
+%     [0.14 0.5815 0.094 0.0675],...
+%     'String',{'$t = 0.0625$'}, 'interpreter', 'latex', 'Fontsize', 18, ...
+%     'Edgecolor', 'None');
+% 
+% annotation('textbox',...
+%     [0.65 0.5815 0.094 0.0675],...
+%     'String',{'$t = 1$'}, 'interpreter', 'latex', 'Fontsize', 18, ...
+%     'Edgecolor', 'None');
 
 %% Figure options
 grid on;
@@ -128,18 +120,19 @@ xlabel("$x$");
 ylabel("$p(x, 0, t)$");
 
 % Set axes limits
-xlim([-0.26, 0.26]);
-ylim([0, 200]);
-xticks([0, 0.05, 0.1, 0.15, 0.2, 0.25]);
+xlim([0, 0.35]);
+ylim([0, 40]);
+% xticks([0, 0.05, 0.1, 0.15, 0.2, 0.25]);
 
 % Legend
-% legend(h(1:3), 'Location', 'North');
+legend(h(1:3), 'Location', 'Northwest');
 
 
 % Set figure position
-set(gcf,'position', [100, 100, 960, 400]);
+set(gcf,'position', [100, 100, 600, 400]);
 
 set(gcf, 'Renderer', 'Painters');
-
+pause(0.1);
+% 
 exportgraphics(gca,'png/CompositePressure2D.png', 'Resolution', 300);
 exportgraphics(gca,'eps/CompositePressure2D.eps', 'Resolution', 300);
