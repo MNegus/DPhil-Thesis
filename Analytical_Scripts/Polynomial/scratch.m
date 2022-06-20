fed08bdc201d9dbd5d838e90978de34577ff2370
Michael <michaelnegus1@gmail.com>
2022-06-20 23:21:06 +0100

    Messing with scratch

--- Analytical_Scripts/Polynomial/scratch.m
+++ Analytical_Scripts/Polynomial/scratch.m
@@ -12,48 +12,27 @@ set(groot, 'DefaultLegendInterpreter', 'latex');
 
 
 %% Parameters
-epsilon = 0.1;
-% tmax = 0.625 / (epsilon^2);
 tmax = 1;
 ts = linspace(0, tmax, 1e3)';
 
 %% Substrate parameters, such that w(x, t) = w(t) * (L^2 - x^2);
-L = 1;
+[epsilon, L, q, omega] = quadraticparameters(); % Substrate parameters
 
-% Just quadratic time dependence
-% q = 0.1;
-% w_coeffs = q * ts.^2;
-% w_t_coeffs = 2 * q * ts;
-% w_tt_coeffs = 2 * q;
+[aQuads, a_tQuads, a_ttQuads, bQuads, b_tQuads, b_ttQuads] ...
+    = quadraticsubstrate(ts, L, q, omega); % Quadratic substrate coefficients
 
-% Oscillatory time dependence (with epsilons)
-% q = 0.15;
-% omega = 250 * epsilon^2;
-% w_coeffs = q * (epsilon^4 * ts.^2 + 1 - cos(omega * ts));
-% w_t_coeffs = q * (2 * epsilon^4 * ts + omega * sin(omega * ts));
-% w_tt_coeffs = q * (2 * epsilon^4 + omega^2 * cos(omega * ts));
-
-% Oscillatory time dependence (without epsilons)
-q = 0.1;
-omega = 2.5;
-w_coeffs = q * (ts.^2 + 1 - cos(omega * ts));
-w_t_coeffs = q * (2 * ts + omega * sin(omega * ts));
-w_tt_coeffs = q * (2 + omega^2 * cos(omega * ts));
-
-% Define substrate coefficients such that w = a + b * x^2
-as = w_coeffs * L^2;
-a_ts = w_t_coeffs * L^2;
-a_tts = w_tt_coeffs * L^2;
-
-bs = - as / L^2;
-b_ts = - a_ts / L^2;
-b_tts = - a_tts / L^2;
+[aFlats, a_tFlats, a_ttFlats, bFlats, b_tFlats, b_ttFlats] ...
+    = flatsubstrate(ts, q, omega); % Flat substrate coefficients
 
 
 %% Load in substrate coefficients
-SubstrateCoefficients ...
-    = substratecoefficients(as, bs, a_ts, b_ts, a_tts, b_tts, epsilon);
-TimeDependents = timedependents(ts, SubstrateCoefficients);
+QuadSubstrateCoefficients ...
+    = substratecoefficients(aQuads, bQuads, a_tQuads, b_tQuads, a_ttQuads, b_ttQuads, epsilon);
+QuadTimeDependents = timedependents(ts, QuadSubstrateCoefficients);
+
+FlatSubstrateCoefficients ...
+    = substratecoefficients(aFlats, bFlats, a_tFlats, b_tFlats, a_ttFlats, b_ttFlats, epsilon);
+FlatTimeDependents = timedependents(ts, FlatSubstrateCoefficients);
 
 %% Load in zero substrate coefficients
 zeroTerm = zeros(size(ts));
@@ -63,8 +42,10 @@ StationarySubstrateCoefficients ...
 StationaryTimeDependents = timedependents(ts, StationarySubstrateCoefficients);
 
 %% Load in forces
-[Fs_composite, Fs_outer, Fs_inner] ...
-    = substrateforce(ts, TimeDependents, epsilon);
+[Quad_Fs_composite, Quad_Fs_outer, Quad_Fs_inner] ...
+    = substrateforce(ts, QuadTimeDependents, epsilon);
+[Flat_Fs_composite, Flat_Fs_outer, Flat_Fs_inner] ...
+    = substrateforce(ts, FlatTimeDependents, epsilon);
 [Stationary_Fs_composite, Stationary_Fs_outer, Stationary_Fs_inner] ...
     = substrateforce(ts, StationaryTimeDependents, epsilon);
 
@@ -74,41 +55,59 @@ figNo = 1;
 %% Plot substrate
 figure(figNo);
 figNo = figNo + 1;
-plot(ts, w_coeffs);
+plot(ts, aQuads);
 title("Substrate");
+
+%% Plot substrate velocity
+figure(figNo);
+figNo = figNo + 1;
+plot(ts, a_tQuads);
+title("Substrate velocity");
+
+%% Plot substrate velocity
+figure(figNo);
+figNo = figNo + 1;
+plot(ts, a_ttQuads);
+title("Substrate acceleration");
 
 %% Plot turnover point
 figure(figNo);
 figNo = figNo + 1;
-ds = TimeDependents.ds;
-dStat = StationaryTimeDependents.ds;
+ds_quad = QuadTimeDependents.ds;
+ds_flat = FlatTimeDependents.ds;
+ds_stat = StationaryTimeDependents.ds;
 hold on;
-plot(ts, dStat);
-plot(ts, ds);
-legend(["Stationary", "Moving"]);
+plot(ts, ds_stat);
+plot(ts, ds_flat);
+plot(ts, ds_quad);
+legend(["Stationary", "Flat", "Quadratic"]);
 title("Turnover point");
 
 
 %% Plot turnover derivative
 figure(figNo);
 figNo = figNo + 1;
-d_ts = TimeDependents.d_ts;
-d_tStat = StationaryTimeDependents.d_ts;
+d_ts_quad = QuadTimeDependents.d_ts;
+d_ts_flat = FlatTimeDependents.d_ts;
+d_ts_stat = StationaryTimeDependents.d_ts;
 hold on;
-plot(ts, d_tStat);
-plot(ts, d_ts);
-legend(["Stationary", "Moving"]);
+plot(ts, d_ts_stat);
+plot(ts, d_ts_flat);
+plot(ts, d_ts_quad);
+legend(["Stationary", "Flat", "Quadratic"]);
 title("Turnover point derivative");
 
 %% Plot jet thickness point
 figure(figNo);
 figNo = figNo + 1;
-Js = TimeDependents.Js;
-JStat = StationaryTimeDependents.Js;
+Js_quad = QuadTimeDependents.Js;
+Js_flat = FlatTimeDependents.Js;
+Js_stat = StationaryTimeDependents.Js;
 hold on;
-plot(ts, JStat);
-plot(ts, Js);
-legend(["Stationary", "Moving"]);
+plot(ts, Js_stat);
+plot(ts, Js_flat);
+plot(ts, Js_quad);
+legend(["Stationary", "Flat", "Quadratic"]);
 title("Jet thickness");
 
 %% Plot outer force
@@ -116,8 +115,9 @@ figure(figNo);
 figNo = figNo + 1;
 hold on;
 plot(ts, Stationary_Fs_outer);
-plot(ts, Fs_outer);
-legend(["Stationary", "Moving"]);
+plot(ts, Flat_Fs_outer);
+plot(ts, Quad_Fs_outer);
+legend(["Stationary", "Flat", "Quadratic"]);
 title("Outer force");
 
 %% Plot composite force
@@ -125,6 +125,37 @@ figure(figNo);
 figNo = figNo + 1;
 hold on;
 plot(ts, Stationary_Fs_composite);
-plot(ts, Fs_composite);
-legend(["Stationary", "Moving"]);
+plot(ts, Flat_Fs_composite);
+plot(ts, Quad_Fs_composite);
+legend(["Stationary", "Flat", "Quadratic"]);
 title("Composite force");
+
+% %% Plot A values
+% figure(figNo);
+% figNo = figNo + 1;
+% hold on;
+% plot(ts, StationaryTimeDependents.As);
+% plot(ts, FlatTimeDependents.As);
+% plot(ts, QuadTimeDependents.As);
+% legend(["Stationary", "Flat", "Quadratic"]);
+% title("A(t)");
+% 
+% %% Plot B values
+% figure(figNo);
+% figNo = figNo + 1;
+% hold on;
+% plot(ts, StationaryTimeDependents.Bs);
+% plot(ts, FlatTimeDependents.Bs);
+% plot(ts, QuadTimeDependents.Bs);
+% legend(["Stationary", "Flat", "Quadratic"]);
+% title("B(t)");
+% 
+% %% Plot C values
+% figure(figNo);
+% figNo = figNo + 1;
+% hold on;
+% plot(ts, StationaryTimeDependents.Cs);
+% plot(ts, FlatTimeDependents.Cs);
+% plot(ts, QuadTimeDependents.Cs);
+% legend(["Stationary", "Flat", "Quadratic"]);
+% title("C(t)");
