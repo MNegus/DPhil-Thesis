980d8f24ca544b6bbdae67593627ad504934b57f
MNegus <michaelnegus1@gmail.com>
2022-06-23 16:47:32 +0100

    Script to generate all figures

---
+++ Analytical_Scripts/Imposed/Two-Dimensional/Figures/GenerateFigures.m
@@ -0,0 +1,14 @@
+
+%% Create figure directories
+for dirName = ["Two-dimensional_Figures", "Axisymmetric_Figures"]
+    mkdir(dirName);
+    mkdir(dirName + "/png");
+    mkdir(dirName + "/eps");
+    mkdir(dirName + "/fig");
+end
+
+for dimension = ["2D", "axi"]
+
+    %% Time dependents
+    TimeDependentsPlot(dimension);
+end
