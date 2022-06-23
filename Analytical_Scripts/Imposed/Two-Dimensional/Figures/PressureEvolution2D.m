eff53b3431a26fcbba9b648c5637d34be35336e4
MNegus <michaelnegus1@gmail.com>
2022-06-23 12:06:04 +0100

    Row output format

--- Analytical_Scripts/Imposed/Two-Dimensional/Figures/PressureEvolution2D.m
+++ Analytical_Scripts/Imposed/Two-Dimensional/Figures/PressureEvolution2D.m
@@ -9,11 +9,19 @@ close all;
 addpath("../");
 addpath("../Pressures");
 
+painters = true;
+
 %% Figure options
 set(0,'defaultTextInterpreter','latex'); %trying to set the default
 set(0,'defaultAxesFontSize', 18);
 set(0, 'defaultAxesTickLabelInterpreter', 'latex');
 set(groot, 'DefaultLegendInterpreter', 'latex');
+
+if painters
+    set(0, 'defaultFigureRenderer', 'painters');
+else
+    set(0, 'defaultFigureRenderer', 'opengl');
+end
 
 %% Load in color map
 mapObj = load("fine_red_blue_cmap.mat");
@@ -28,7 +36,7 @@ redCol = cmap(end, :);
 tmax = 1;
 ts = linspace(0.05, tmax, 1000)';
 xMax = 2 * epsilon * sqrt(tmax);
-xs = linspace(0, xMax, 1e4);
+noPoints = 1e3;
 
 %% Load in substrate functions
 StationaryFunctions = substratefunctions("stationary");
@@ -46,12 +54,24 @@ typeArr = ["stationary", "flat", "curved"];
 
 freq = 100; % How frequent the lines are
 
-tiledlayout(1, 3);
+tiledlayout(3, 1);
 
 tileNo = 1;
 for typeIdx = 1 : length(typeArr)
     type = typeArr(typeIdx);
     
+    % Set line colors
+    if type == "stationary"
+            lineColor = 'black';
+            
+    elseif type == "flat"
+        lineColor = redCol;
+
+    else
+        lineColor = blueCol;
+    end
+    
+    % Load substrate functions
     SubstrateFunctions = FunctionsArray(typeIdx);
     
     % Move to next tile
@@ -61,16 +81,21 @@ for typeIdx = 1 : length(typeArr)
     
     for k = 1 : freq : length(ts)
         t = ts(k);
-        if type == "stationary"
-            lineColor = 'black';
-            
-        elseif type == "flat"
-            lineColor = redCol;
-            
-        else
-            lineColor = blueCol;
-        end
 
+        %% Determine x values, clustered around the maximum pressure point
+        % Find maximum pressure point
+        [~, x_pMax] = pressuremax(t, SubstrateFunctions, epsilon);
+        
+        fineWidth = 1e-3;
+        leftWidth = x_pMax/ xMax;
+        rightWidth = (xMax - x_pMax) / xMax;
+        xsLower = linspace(0, x_pMax - fineWidth, floor(leftWidth * noPoints));
+        xsFine = linspace(x_pMax - fineWidth, x_pMax + fineWidth, 100);
+            
+        xsUpper = linspace(x_pMax + fineWidth, xMax, floor(rightWidth * noPoints));
+        
+        xs = [xsLower, xsFine, xsUpper];
+        
         % Load in composite pressure
         [ps, ~, ~] = substratepressure(xs, t, SubstrateFunctions, epsilon);
 
@@ -80,25 +105,26 @@ for typeIdx = 1 : length(typeArr)
     end
     
     %% Plot maximum pressure
-    [pMaxs, xMaxs] = pressuremax(ts, SubstrateFunctions, epsilon);
+    tLongs = linspace(0, 2 * tmax);
+    [pMaxs, xMaxs] = pressuremax(tLongs, SubstrateFunctions, epsilon);
     plot(xMaxs, pMaxs, 'color', lineColor, 'linestyle', '--');
     
     %% Tile figure settings
     xlim([0, xMax]);
     ylim([-10, 300]);
     xlabel("$x$");
-    ylabel("$p(x, t)$");
+%     ylabel("$p_{\textrm{comp}}(x, t)$");
+    ylabel("$p_{{comp}}(x, t)$");
     grid on;
     
 end
 
 %% Figure settings
-set(gcf,'position', [100, 100, 960, 300]);
+set(gcf,'position', [100, 100, 800, 600]);
 pause(0.1);
 
-% set(gcf, 'Renderer', 'Painters');
-
 % Export figure
-% savefig(gcf, 'fig/PressureEvolution2D.fig');
-% exportgraphics(gcf,'png/PressureEvolution2D.png', 'Resolution', 300);
-% exportgraphics(gcf,'eps/PressureEvolution2D.eps', 'Resolution', 300);
+filename = "PressureEvolution2D";
+savefig(gcf, sprintf("fig/%s.fig", filename));
+exportgraphics(gcf, sprintf("png/%s.png", filename), 'Resolution', 300);
+exportgraphics(gcf,sprintf("eps/%s.eps", filename), 'Resolution', 300);
