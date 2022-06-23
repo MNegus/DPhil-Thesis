f58c07dd60c62a44d957234b17c816955f4ee252
MNegus <michaelnegus1@gmail.com>
2022-06-23 16:44:53 +0100

    Converting to script with optional dimension

--- Analytical_Scripts/Imposed/Two-Dimensional/Figures/TimeDependentsPlot.m
+++ Analytical_Scripts/Imposed/Two-Dimensional/Figures/TimeDependentsPlot.m
@@ -1,221 +1,223 @@
-%%TimeDependents2D.m
+function TimeDependentsPlot(dimension)
+%TIMEDEPENDENTSPLOT Function to plot the time-dependent quantities
 %   Script to make figures comparing the time-dependent quantities (i.e.
-%   substrate position, turnover point, jet thickness etc. for a stationary
-%   substrate, a flat moving and a quadratic moving substrate.
+%   substrate position, turnover point, jet thickness etc. for either 2D
+%   (where dimension == '2D') or axisymmetric (where dimension == 'axi').
 
-clear;
-close all;
+    close all;
 
-addpath("../");
-addpath("../Forces");
-addpath("../Energies");
+    addpath("../");
+    addpath("../Forces");
+    addpath("../Energies");
 
-%% Figure options
-set(0,'defaultTextInterpreter','latex'); %trying to set the default
-set(0,'defaultAxesFontSize', 18);
-set(0, 'defaultAxesTickLabelInterpreter', 'latex');
-set(0, 'defaultFigureRenderer', 'painters');
-set(groot, 'DefaultLegendInterpreter', 'latex');
+    %% Figure options
+    set(0,'defaultTextInterpreter','latex'); %trying to set the default
+    set(0,'defaultAxesFontSize', 18);
+    set(0, 'defaultAxesTickLabelInterpreter', 'latex');
+    set(0, 'defaultFigureRenderer', 'painters');
+    set(groot, 'DefaultLegendInterpreter', 'latex');
 
-%% Load in color map
-mapObj = load("fine_red_blue_cmap.mat");
-cmap = mapObj.cmap;
-blueCol = cmap(1, :);
-redCol = cmap(end, :);
+    %% Load in color map
+    mapObj = load("fine_red_blue_cmap.mat");
+    cmap = mapObj.cmap;
+    blueCol = cmap(1, :);
+    redCol = cmap(end, :);
+    blackCol = [0, 0, 0];
 
-%% Load parameters
+    %% Substrate functions
+    if dimension == "2D"
+        types = ["stationary", "flat", "curved"];
+        colors = [blackCol; redCol; blueCol];
+        displayNames = ["Stationary substrate", "Flat substrate", "Curved substrate"];
+        dirName = "Two-dimensional_Figures";
+    elseif dimension == "axi"
+        types = ["stationary", "flat"];
+        colors = [blackCol; redCol];
+        displayNames = ["Stationary substrate", "Flat substrate"];
+        dirName = "Axisymmetric_Figures";
+    else
+        error("Invalid dimension");
+    end
+    
+    % Load substrate functions
+    for typeIdx = 1 : length(types)
+        SubstrateFunctions(typeIdx) = substratefunctions(types(typeIdx), dimension);
+    end
 
-% Substrate parameters
-[epsilon, L, q, omega] = substrateparameters(); % Substrate parameters
+    %% Load parameters
+    % Load epsilon
+    [epsilon, ~, ~, ~] = substrateparameters(); 
 
-tmax = 1;
-ts = linspace(0, tmax, 1e3)';
+    % Set times
+    tmax = 1;
+    ts = linspace(0, tmax, 1e3)';
 
-%% Load in substrate functions
-dimension = "2D";
-StationaryFunctions = substratefunctions("stationary", dimension);
-FlatFunctions = substratefunctions("flat", dimension);
-CurvedFunctions = substratefunctions("curved", dimension);
+    %% Substrate motion plot
+    fig = tiledlayout(1, 3);
 
-%% Determine forces
-% Stationary substrate forces
-[Stationary_Fs_composite, Stationary_Fs_outer, Stationary_Fs_inner] ...
-    = substrateforce(ts, StationaryFunctions);
+    % Need the flat functions struct
+    FlatFunctions = substratefunctions('flat', dimension);
+    
+    % Substrate position
+    nexttile;
+    ws = FlatFunctions.a(ts);
+    plot(ts, ws, 'color', 'black', 'linewidth', 2);
+    xlabel("$t$");
+    ylabel("$w(t$)");
+    grid on;
 
-% Flat substrate time dependents
-[Flat_Fs_composite, Flat_Fs_outer, Flat_Fs_inner] ...
-    = substrateforce(ts, FlatFunctions);
+    % Substrate velocity
+    nexttile;
+    w_ts = FlatFunctions.a_t(ts);
+    plot(ts, w_ts / epsilon, 'color', 'black', 'linewidth', 2);
+    xlabel("$t$");
+    ylabel("$w'(t) / \epsilon$");
+    grid on;
 
-% Quadratic substrate time dependents
-[Curved_Fs_composite, Curved_Fs_outer, Curved_Fs_inner] ...
-    = substrateforce(ts, CurvedFunctions);
+    % Substrate acceleration
+    nexttile;
+    w_tts = FlatFunctions.a_tt(ts);
+    plot(ts, w_tts / epsilon^2, 'color', 'black', 'linewidth', 2);
+    xlabel("$t$");
+    ylabel("$w''(t) / \epsilon^2$");
+    grid on;
 
-%% Determine energies
-% Stationary substrate energies
-[Stationary_Es_outer, Stationary_Es_jets] ...
-    = dropletenergy(ts, StationaryFunctions);
+    % Figure settings
+    set(gcf,'position', [100, 100, 960, 300]);
+    pause(0.1);
 
-% Stationary substrate energies
-[Flat_Es_outer, Flat_Es_jets] ...
-    = dropletenergy(ts, FlatFunctions);
+    % Export figure
+    filename = sprintf("AnalyticalImposed_%s", dimension);
+    savefig(gcf, sprintf("%s/fig/%s.fig", dirName, filename));
+    exportgraphics(gcf, sprintf("%s/png/%s.png", dirName, filename), 'Resolution', 300);
+    exportgraphics(gcf,sprintf("%s/eps/%s.eps", dirName, filename), 'Resolution', 300);
 
-% Stationary substrate energies
-[Curved_Es_outer, Curved_Es_jets] ...
-    = dropletenergy(ts, CurvedFunctions);
+    %% Turnover point plot
+    figNo = 2;
+    figure(figNo);
+    figNo = figNo + 1;
+    hold on;
+    for typeIdx = 1 : length(types)
+        plot(ts, SubstrateFunctions(typeIdx).d(ts), 'color', colors(typeIdx, :), ...
+            'linewidth', 2);
+    end
+    legend(displayNames, 'Location', 'southeast');
+    xlabel("$t$");
+    ylabel("$d_0(t)$");
+    grid on;
 
-%% Substrate motion plot
-fig = tiledlayout(1, 3);
+    % Figure settings
+    set(gcf,'position', [100, 100, 600, 300]);
+    pause(0.1);
 
-% Substrate position
-nexttile;
-ws = FlatFunctions.a(ts);
-plot(ts, ws, 'color', 'black', 'linewidth', 2);
-xlabel("$t$");
-ylabel("$w(t$)");
-grid on;
+    % Export figure
+    filename = sprintf("TurnoverPoints_%s", dimension);
+    savefig(gcf, sprintf("%s/fig/%s.fig", dirName, filename));
+    exportgraphics(gcf, sprintf("%s/png/%s.png", dirName, filename), 'Resolution', 300);
+    exportgraphics(gcf,sprintf("%s/eps/%s.eps", dirName, filename), 'Resolution', 300);
 
-% Substrate velocity
-nexttile;
-w_ts = FlatFunctions.a_t(ts);
-plot(ts, w_ts / epsilon, 'color', 'black', 'linewidth', 2);
-xlabel("$t$");
-ylabel("$w'(t) / \epsilon$");
-grid on;
+    %% Jet thickness
+    figure(figNo);
+    figNo = figNo + 1;
+    hold on;
+    for typeIdx = 1 : length(types)
+        plot(ts, SubstrateFunctions(typeIdx).J(ts), 'color', colors(typeIdx, :), ...
+            'linewidth', 2);
+    end
 
-% Substrate acceleration
-nexttile;
-w_tts = FlatFunctions.a_tt(ts);
-plot(ts, w_tts / epsilon^2, 'color', 'black', 'linewidth', 2);
-xlabel("$t$");
-ylabel("$w''(t) / \epsilon^2$");
-grid on;
+    legend(displayNames, 'Location', 'northwest');
+    xlabel("$t$");
+    ylabel("$J(t)$");
+    grid on;
 
-% Figure settings
-set(gcf,'position', [100, 100, 960, 300]);
-pause(0.1);
+    % Figure settings
+    set(gcf,'position', [100, 100, 600, 300]);
+    pause(0.1);
 
-% Export figure
-filename = "AnalyticalImposed";
-savefig(gcf, sprintf("fig/%s.fig", filename));
-exportgraphics(gcf, sprintf("png/%s.png", filename), 'Resolution', 300);
-exportgraphics(gcf,sprintf("eps/%s.eps", filename), 'Resolution', 300);
+    % Export figure
+    filename = sprintf("JetThickness_%s", dimension);
+    savefig(gcf, sprintf("%s/fig/%s.fig", dirName, filename));
+    exportgraphics(gcf, sprintf("%s/png/%s.png", dirName, filename), 'Resolution', 300);
+    exportgraphics(gcf,sprintf("%s/eps/%s.eps", dirName, filename), 'Resolution', 300);
 
-%% Turnover point plot
-figNo = 2;
-figure(figNo);
-figNo = figNo + 1;
-hold on;
-plot(ts, StationaryFunctions.d(ts), 'color', 'black', 'linewidth', 2);
-plot(ts, FlatFunctions.d(ts), 'color', redCol, 'linewidth', 2);
-plot(ts, CurvedFunctions.d(ts), 'color', blueCol, 'linewidth', 2);
+    %% Force plot
 
-legend(["Stationary substrate", "Flat substrate", "Curved substrate"], ...
-    'Location', 'southeast');
-xlabel("$t$");
-ylabel("$d_0(t)$");
-grid on;
+    figure(figNo);
+    figNo = figNo + 1;
+    hold on;
 
-% Figure settings
-set(gcf,'position', [100, 100, 600, 300]);
-pause(0.1);
+    for typeIdx = 1 : length(types)
+        % Load forces
+        [Fs_composite, Fs_outer, ~] ...
+            = substrateforce(ts, SubstrateFunctions(typeIdx));
+        
+        % Plot composite
+        h(typeIdx) = plot(ts, Fs_composite, 'color', colors(typeIdx, :), ...
+            'linewidth', 2, 'Displayname', displayNames(typeIdx));
+       
+        % Plot outer
+        plot(ts, Fs_outer, 'color', colors(typeIdx, :), ...
+            'linewidth', 2, 'linestyle', '--');
+    end
 
-% Export figure
-filename = "TurnoverPoints2D";
-savefig(gcf, sprintf("fig/%s.fig", filename));
-exportgraphics(gca, sprintf("png/%s.png", filename), 'Resolution', 300);
-exportgraphics(gca,sprintf("eps/%s.eps", filename), 'Resolution', 300);
+    legend(h(1 : length(types)), 'Location', 'northwest');
 
-%% Jet thickness
-figure(figNo);
-figNo = figNo + 1;
-hold on;
-plot(ts, StationaryFunctions.J(ts), 'color', 'black', 'linewidth', 2);
-plot(ts, FlatFunctions.J(ts), 'color', redCol, 'linewidth', 2);
-plot(ts, CurvedFunctions.J(ts), 'color', blueCol, 'linewidth', 2);
+    xlabel("$t$");
+    ylabel("$F(t)$");
 
-legend(["Stationary substrate", "Flat substrate", "Curved substrate"], ...
-    'Location', 'northwest');
-xlabel("$t$");
-ylabel("$J(t)$");
-grid on;
+    if dimension == "2D"
+        ylim([0, 15]);
+    else
+        ylim([0, 2]);
+    end
+    grid on;
 
-% Figure settings
-set(gcf,'position', [100, 100, 600, 300]);
-pause(0.1);
+    % Figure settings
+    set(gcf,'position', [100, 100, 600, 300]);
+    pause(0.1);
 
-% Export figure
-filename = "JetThickness2D";
-savefig(gcf, sprintf("fig/%s.fig", filename));
-exportgraphics(gca, sprintf("png/%s.png", filename), 'Resolution', 300);
-exportgraphics(gca,sprintf("eps/%s.eps", filename), 'Resolution', 300);
+    % Export figure
+    filename = sprintf("Force_%s", dimension);
+    savefig(gcf, sprintf("%s/fig/%s.fig", dirName, filename));
+    exportgraphics(gcf, sprintf("%s/png/%s.png", dirName, filename), 'Resolution', 300);
+    exportgraphics(gcf,sprintf("%s/eps/%s.eps", dirName, filename), 'Resolution', 300);
 
-%% Force plot
+    %% Energy plot
+    figure(figNo);
+    figNo = figNo + 1;
+    hold on;
 
-figure(figNo);
-figNo = figNo + 1;
-hold on;
+    for typeIdx = 1 : length(types)
+        % Load energies
+        [Es_outer, Es_jets] = dropletenergy(ts, SubstrateFunctions(typeIdx));
+        
+        % Plot outer
+        h(typeIdx) = plot(ts, Es_outer, 'color', colors(typeIdx, :), ...
+            'linewidth', 1.5, 'Displayname', displayNames(typeIdx));
+       
+        % Plot jets
+        plot(ts, Es_jets, 'color', colors(typeIdx, :), ...
+            'linewidth', 3, 'linestyle', '--');
+    end
 
-% Plot composite forces
-plot(ts, Stationary_Fs_composite, 'color', 'black', 'linewidth', 2);
-plot(ts, Flat_Fs_composite, 'color', redCol, 'linewidth', 2);
-plot(ts, Curved_Fs_composite, 'color', blueCol, 'linewidth', 2);
+    legend(h(1 : length(types)), 'Location', 'northwest');
+    xlabel("$t$");
+    ylabel("$E_{K}(t)$");
+    grid on;
+    
+    if dimension == "2D"
+        ylim([0, 0.035]);
+    else
+        ylim([0, 0.004]);
+    end
 
-% Plot outer forces
-plot(ts, Stationary_Fs_outer, 'color', 'black', 'linewidth', 2, 'linestyle', '--');
-plot(ts, Flat_Fs_outer, 'color', redCol, 'linewidth', 2, 'linestyle', '--');
-plot(ts, Curved_Fs_outer, 'color', blueCol, 'linewidth', 2, 'linestyle', '--');
+    % Figure settings
+    set(gcf,'position', [100, 100, 600, 300]);
+    pause(0.1);
 
-lh = legend(["Stationary substrate", "Flat substrate", "Curved substrate"], ...
-    'Location', 'northwest');
-% lh.Position(1) = 0.52 - lh.Position(3)/2; 
-% lh.Position(2) = 0.575 - lh.Position(4)/2;
-
-xlabel("$t$");
-ylabel("$F(t)$");
-
-ylim([0, 15]);
-grid on;
-
-% Figure settings
-set(gcf,'position', [100, 100, 600, 300]);
-pause(0.1);
-
-% Export figure
-filename = "Force2D";
-savefig(gcf, sprintf("fig/%s.fig", filename));
-exportgraphics(gca, sprintf("png/%s.png", filename), 'Resolution', 300);
-exportgraphics(gca,sprintf("eps/%s.eps", filename), 'Resolution', 300);
-
-%% Energy plot
-
-figure(figNo);
-figNo = figNo + 1;
-hold on;
-
-% Plot outer energies
-plot(ts, Stationary_Es_outer, 'color', 'black', 'linewidth', 1.5);
-plot(ts, Flat_Es_outer, 'color', redCol, 'linewidth', 1.5);
-plot(ts, Curved_Es_outer, 'color', blueCol, 'linewidth', 1.5);
-
-% Plot jet energies
-plot(ts, Stationary_Es_jets, 'color', 'black', 'linewidth', 3, 'linestyle', '--');
-plot(ts, Flat_Es_jets, 'color', redCol, 'linewidth', 3, 'linestyle', '--');
-plot(ts, Curved_Es_jets, 'color', blueCol, 'linewidth', 3, 'linestyle', '--');
-
-legend(["Stationary substrate", "Flat substrate", "Curved substrate"], ...
-    'Location', 'northwest');
-xlabel("$t$");
-ylabel("$E_{K}(t)$");
-grid on;
-ylim([0, 0.035]);
-
-% Figure settings
-set(gcf,'position', [100, 100, 600, 300]);
-pause(0.1);
-
-% Export figure
-filename = "Energy2D";
-savefig(gcf, sprintf("fig/%s.fig", filename));
-exportgraphics(gca, sprintf("png/%s.png", filename), 'Resolution', 300);
-exportgraphics(gca,sprintf("eps/%s.eps", filename), 'Resolution', 300);
-
+    % Export figure
+    filename = sprintf("Energy_%s", dimension);
+    savefig(gcf, sprintf("%s/fig/%s.fig", dirName, filename));
+    exportgraphics(gcf, sprintf("%s/png/%s.png", dirName, filename), 'Resolution', 300);
+    exportgraphics(gcf,sprintf("%s/eps/%s.eps", dirName, filename), 'Resolution', 300);
+end
