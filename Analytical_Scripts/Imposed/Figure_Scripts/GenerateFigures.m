c34c6b38bddfbc76be74780cb0857486d5c9a7bf
MNegus <michaelnegus1@gmail.com>
2022-06-29 08:46:55 +0100

    Adding in remaining scripts

--- Analytical_Scripts/Imposed/Figure_Scripts/GenerateFigures.m
+++ Analytical_Scripts/Imposed/Figure_Scripts/GenerateFigures.m
@@ -7,11 +7,30 @@ for dirName = ["Two-dimensional_Figures", "Axisymmetric_Figures"]
     mkdir(dirName + "/fig");
 end
 
+%% Loop over dimensions
 for dimension = ["2D", "axi"]
+% for dimension = "axi"
 
     %% Time dependents
     TimeDependentsPlot(dimension);
     
     %% Jet evolution
     JetEvolutionPlot(dimension);
+    
+    %% Composite pressure 
+    CompositePressurePlot(dimension);
+    
+    %% Pressure evolution
+    PressureEvolutionPlot(dimension);
+
+    %% Outer free surface plot
+%     OuterFreeSurfacePlot(dimension);
+
+    %% Outer outer pressure
+%     OuterOuterPressurePlot(dimension);
+
+
 end
+
+%% Single dimension plots
+OuterStreamlinePressure2D();
