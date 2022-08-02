1e2da9d929690a7ae1d0d89b7165017d66040dc1
Michael <michaelnegus1@gmail.com>
2022-08-02 16:46:40 +0100

    Removing old plate scripts

--- Analytical_Scripts/Plate/Two-dimensional/Forces/overlapforce.m
+++
@@ -1,7 +0,0 @@
-function Fs = overlapforce(Cs)
-%%innerforce
-%   Returns the overlap solution for the force on the substrate in the
-%   two-dimensional, plate impact case.
-    
-    Fs = 2 * sqrt(2) * Cs;
-end
