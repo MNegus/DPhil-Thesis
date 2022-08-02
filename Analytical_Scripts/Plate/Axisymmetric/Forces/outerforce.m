632f2c1f9a81dce1fa2687607f58a5b8ca90a343
Michael <michaelnegus1@gmail.com>
2022-08-02 16:46:40 +0100

    Removing old plate scripts

--- Analytical_Scripts/Plate/Axisymmetric/Forces/outerforce.m
+++
@@ -1,8 +0,0 @@
-function Fs = outerforce(ts, ws, w_ts, w_tts, epsilon)
-%%outerforce
-%   Returns the outer solution for the force on the substrate in the
-%   axisymmetric-dimensional, plate impact case.
-
-    Fs = 2 * sqrt(3) * epsilon ...
-        * (3 * (1 - w_ts).^2 - 2 * w_tts .* (ts - ws)) .* sqrt(ts - ws);
-end
--- Analytical_Scripts/Plate/Two-dimensional/Forces/outerforce.m
+++
@@ -1,7 +0,0 @@
-function Fs = outerforce(ds, As, w_tts)
-%%outerforce
-%   Returns the outer solution for the force on the substrate in the
-%   two-dimensional, plate impact case.
-
-    Fs = pi * As - w_tts .* ds.^3 / 3;
-end
