58b80c0d7ee3a028d5f2821fc6ca7df85f50aa4d
Michael <michaelnegus1@gmail.com>
2022-08-02 17:06:17 +0100

    Removing deprecated axisymmetric scripts

--- Analytical_Scripts/Imposed/Deprecated_Axi_code/Pressures/compositepressure.m
+++
@@ -1,6 +0,0 @@
-function ps = compositepressure(ps_outer, ps_inner, ps_overlap)
-%%compositepressure
-%   Returns the additive composite expansion of the pressure along the
-%   substrate.
-    ps = ps_outer + ps_inner - ps_overlap;
-end
--- Analytical_Scripts/Imposed/Deprecated_Axi_code/plateparameters.m
+++
@@ -1,9 +0,0 @@
-function [epsilon, L, q, omega] = plateparameters()
-%PLATEPARAMETERS Set consistent parameters for imposed plate.
-
-    epsilon = 0.1;
-    L = 0.25;
-    q = 0.15;
-	omega = 2.5;
-end
-
