9b451a06460c0c1ff7ef2e1283623b0dd7a1ef44
Michael <michaelnegus1@gmail.com>
2022-08-04 13:38:20 +0100

    Script for combined composite solution for free surface

---
+++ Analytical_Scripts/FreeSurface/outer_inner_jet_freesurface_composite.m
@@ -0,0 +1,40 @@
+function [xs, hs] = outer_inner_jet_freesurface_composite(xMax, t, SubstrateFunctions)
+%OUTER_INNER_JET_COMPOSITE Composite solution across outer, inner and jet
+%regions of the free surface.
+%   Constructs the composite solution of the free surface across the outer,
+%   inner and jet regions. Consists of a piecewise solution, where the
+%   upper part is the composite between the outer and the upper side of the
+%   inner solution, and the lower part is the composite between the inner
+%   and the jet region. User needs to specify xMax, which is the maximum
+%   value of x chosen. The function will automatically discretise with
+%   approximately 1000 points per side.
+%   CURRENTLY ONLY SUPPORTED FOR AXISYMMETRIC SOLUTION. THIS COULD BE
+%   CHANGED IF THE OUTER-INNER COMPOSITE IS SET TO BE FROM THE INNER
+%   SOLUTION, WHICH WILL BE DONE LATER.
+
+    %% Load substrate dependent values
+    d = SubstrateFunctions.d(t);
+    J = SubstrateFunctions.J(t);
+    epsilon = SubstrateFunctions.epsilon;
+    
+    %% Outer-inner composite
+    xsUpper = linspace(epsilon * d, xMax, 1e3);
+    
+    hsOuter = outerfreesurface(xsUpper, t, SubstrateFunctions);
+    hsInnerUpper = innerfreesurface_upper(xsUpper, t, SubstrateFunctions);
+    hsOverlapOuter = inner_outer_overlapfreesurface(xsUpper, t, SubstrateFunctions);
+    
+    hsCompositeOuter = hsOuter + hsInnerUpper - hsOverlapOuter;
+    
+    %% Inner-jet composite
+    [xsLower, hsJet] = jetfreesurface(xMax, t, SubstrateFunctions);
+    hsInnerLower = innerfreesurface_lower(xsLower, t, SubstrateFunctions);
+    
+    % Overlap just set to the jet thickness, needs checking
+    hsCompositeJet = hsInnerLower + hsJet - epsilon^3 * J;
+    
+    %% Combine solutions
+    xs = [xsLower, xsUpper];
+    hs = [hsCompositeJet, hsCompositeOuter];
+
+end
