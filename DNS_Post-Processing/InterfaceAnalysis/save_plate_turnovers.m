ae2af65d593a903918830f9fd5d221a75269377b
Michael <michaelnegus1@gmail.com>
2022-08-11 10:05:58 +0100

    Reverting to validation run type

--- DNS_Post-Processing/InterfaceAnalysis/save_plate_turnovers.m
+++ DNS_Post-Processing/InterfaceAnalysis/save_plate_turnovers.m
@@ -4,8 +4,7 @@ function save_plate_turnovers(master_dir)
     close all;
 
     %% Find a couple of examples
-%     max_levels = 10 : 14;
-    max_levels = 13;
+    max_levels = 10 : 14;
 %     master_dir = "/scratch/negus/stationary_plate_maxlevel_validation";
 
     %% Options
@@ -20,8 +19,7 @@ function save_plate_turnovers(master_dir)
     %% Loop over both the 2D and the axisymmetric cases
     for axi = [0, 1]
         % Name of parent directory
-%         parent_dir = sprintf("%s/axi_%d", master_dir, axi);
-        parent_dir = master_dir;
+        parent_dir = sprintf("%s/axi_%d", master_dir, axi);
 
         %% Loop over all the levels
         for m = 1 : length(max_levels)
@@ -29,8 +27,7 @@ function save_plate_turnovers(master_dir)
             Delta = Deltas(m);
 
             % Directories for this level
-%             level_dir = sprintf("%s/max_level_%d", parent_dir, max_level);
-            level_dir = parent_dir;
+            level_dir = sprintf("%s/max_level_%d", parent_dir, max_level);
             interface_dir = append(level_dir, "/interfaces");
 
             % Find the turnover points
