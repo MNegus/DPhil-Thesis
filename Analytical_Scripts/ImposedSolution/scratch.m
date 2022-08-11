9ad49268c0cb754a63ba197e2b62678dd7ada71a
Michael <michaelnegus1@gmail.com>
2022-08-11 09:59:47 +0100

    Experimenting with free surface plotting

--- Analytical_Scripts/ImposedSolution/scratch.m
+++ Analytical_Scripts/ImposedSolution/scratch.m
@@ -5,53 +5,43 @@ addpath("../FreeSurface");
 SubstrateFunctions = imposedsubstratefunctions("stationary", "axi");
 
 %% Parameters
-ts = linspace(0.001, 1, 1e2);
-figure(1);
+ts = linspace(0.001, 10, 1e2);
+
+tiledlayout(1, 2);
 for t = ts
 % for t = 0.5
     
     d = SubstrateFunctions.d(t);
     epsilon = SubstrateFunctions.epsilon;
-    J = SubstrateFunctions.J(t);
-    xMax = 1.1 * epsilon * d;
-    xs = linspace(epsilon * d, xMax, 1e3);
+    xMaxUpper = 1;
+    xMaxLower = 1.5 * epsilon * d;
 
-    %% Outer composite
-    hsOuter = outerfreesurface(xs, t, SubstrateFunctions);
-    hsInnerUpper = innerfreesurface_upper(xs, t, SubstrateFunctions);
-    hsOverlapOuter = inner_outer_overlapfreesurface(xs, t, SubstrateFunctions);
-
-    % Composite solution
-    hsCompositeOuter = hsOuter + hsInnerUpper - hsOverlapOuter;
-    
-    %% Jet composite
-    [xsJet, hsJet] = jetfreesurface(xMax, t, SubstrateFunctions);
-    hsInnerLower = innerfreesurface_lower(xsJet, t, SubstrateFunctions);
-    hsOverlapJet = jet_inner_overlapfreesurface(xsJet, t, SubstrateFunctions);
-%     hsCompositeJet = hsInnerLower + hsJet - hsOverlapJet;
-    hsCompositeJet = hsInnerLower + hsJet - epsilon^3 * J;
-    
-    %% Plot
-    
-%     plot(xs, hsOuter, 'linewidth', 2);
-%     hold on;
-%     plot(xsInner, hsInner, 'linewidth', 2);
-%     plot(xs, hsCompositeOuter, 'linewidth', 2, 'color', 'black');
-%     plot(xsJet, hsJet, 'linewidth', 2);
-%     hold off;
-    plot(xs, hsCompositeOuter);
+    %% Outer outer solution
+    [xsLower, hsLower, xsUpper, hsUpper] = outerouterfreesurface(t, SubstrateFunctions);
+    plot(xsLower, hsLower);
     hold on;
-%     plot(xsJet, hsInnerLower, '-o');
-%     plot(xsJet, hsJet, '-o');
-%     plot(xsJet, hsOverlapJet, '-o');
-    plot(xsJet, hsCompositeJet);
+    plot(xsUpper, hsUpper);
     hold off;
-
-
-    xlim([epsilon * d - 0.005, epsilon * d + 0.015]);
-    ylim([0, 0.02]);
-    pbaspect([1 1 1]);
     
+    xlim([0, 2]);
+    ylim([0, 2]);
+    pbaspect([1 1 1]);
+    %% Load composite solution
+%     [xs, hsTurnover, hsFull] = outer_inner_jet_freesurface_composite(xMaxUpper, xMaxLower, t, SubstrateFunctions);
+    
+    %% Plot solutions
+%     nexttile(1);
+%     plot(xs, hsFull);
+%     xlim([0, 1]);
+%     ylim([0, 1]);  
+%     pbaspect([1 1 1]);
+%     
+%     nexttile(2);
+%     plot(xs, hsTurnover);
+%     xlim([epsilon * d - 0.005, epsilon * d + 0.015]);
+%     ylim([0, 0.02]);
+%     pbaspect([1 1 1]);
+%     
     drawnow;
     pause(0.1);
    
