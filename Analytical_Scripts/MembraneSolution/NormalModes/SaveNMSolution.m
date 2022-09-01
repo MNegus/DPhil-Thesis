6f524d1f66fee3416755bbd6ede031e8f867cd1a
Michael <michaelnegus1@gmail.com>
2022-09-01 20:22:58 +0100

    Updating normal modes solutions

--- Analytical_Scripts/MembraneSolution/NormalModes/SaveNMSolution.m
+++ Analytical_Scripts/MembraneSolution/NormalModes/SaveNMSolution.m
@@ -12,7 +12,7 @@ function SaveNMSolution(data_dir, alpha, beta, gamma, epsilon, N, L, tmax, delta
         = NormalModesODE(alpha, beta, gamma, epsilon, delta_d, d_max, N, L);
 
     %% Converts solution to t-form
-    [ds, as, a_ts, a_tts, q_ts] = NormalModesTemporalForm(ts, ...
+    [ds, as, a_ts, a_tts, q_ts, d_ts, Js] = NormalModesTemporalForm(ts, ...
         t_vals_d_form, d_vals_d_form, as_d_form, a_ts_d_form, kvals, alpha, delta_t);
     
     %% Saves matrices in data_dir
@@ -23,6 +23,8 @@ function SaveNMSolution(data_dir, alpha, beta, gamma, epsilon, N, L, tmax, delta
     SolStruct.a_ts = a_ts;
     SolStruct.a_tts = a_tts;
     SolStruct.q_ts = q_ts;
+    SolStruct.d_ts = d_ts;
+    SolStruct.Js = Js;
 
     save(sprintf("%s/SolStruct.mat", data_dir), 'SolStruct');
 end
--- Analytical_Scripts/MembraneSolution/NormalModes/SaveValidatedNMSolution.m
+++ Analytical_Scripts/MembraneSolution/NormalModes/SaveValidatedNMSolution.m
@@ -1,17 +1,19 @@
 function SaveValidatedNMSolution(data_dir, alpha, beta, gamma, epsilon, L, tmax, delta_t)
 
-    [N, delta_d, ts, ds, as, a_ts, a_tts, q_ts] ...
+    [N, delta_d, ts, ds, as, a_ts, a_tts, q_ts, d_ts, Js] ...
         = ValidatedNMSolution(alpha, beta, gamma, epsilon, L, tmax, delta_t);
     
     %% Saves matrices in data_dir
     SolStruct.N = N;
-    SoltStruct.delta_d = delta_d;
+    SolStruct.delta_d = delta_d;
     SolStruct.ts = ts;
     SolStruct.ds = ds;
     SolStruct.as = as;
     SolStruct.a_ts = a_ts;
     SolStruct.a_tts = a_tts;
     SolStruct.q_ts = q_ts;
+    SolStruct.d_ts = d_ts;
+    SolStruct.Js = Js;
 
     save(sprintf("%s/SolStruct.mat", data_dir), 'SolStruct');
 
