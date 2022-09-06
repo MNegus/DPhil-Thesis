235739cca9679977406f1ab1f2ed3c9ff255cc25
Michael <michaelnegus1@gmail.com>
2022-09-06 16:47:06 +0100

    Adding energy output

--- Analytical_Scripts/MembraneSolution/NormalModes/SaveNMSolution.m
+++ Analytical_Scripts/MembraneSolution/NormalModes/SaveNMSolution.m
@@ -8,23 +8,27 @@ function SaveNMSolution(data_dir, alpha, beta, gamma, epsilon, N, L, tmax, delta
     delta_d = delta_t;
 
     %% Solves the ode in d-form
-    [t_vals_d_form, d_vals_d_form, as_d_form, a_ts_d_form, kvals] ...
+    NM_Struct_d_form ...
         = NormalModesODE(alpha, beta, gamma, epsilon, delta_d, d_max, N, L);
 
     %% Converts solution to t-form
-    [ds, as, a_ts, a_tts, q_ts, d_ts, Js] = NormalModesTemporalForm(ts, ...
-        t_vals_d_form, d_vals_d_form, as_d_form, a_ts_d_form, kvals, alpha, delta_t);
+    NM_Struct = NormalModesTemporalForm(ts, ...
+        NM_Struct_d_form, alpha, delta_t);
     
     %% Saves matrices in data_dir
     SolStruct.N = N;
-    SolStruct.ts = ts;
-    SolStruct.ds = ds;
-    SolStruct.as = as;
-    SolStruct.a_ts = a_ts;
-    SolStruct.a_tts = a_tts;
-    SolStruct.q_ts = q_ts;
-    SolStruct.d_ts = d_ts;
-    SolStruct.Js = Js;
+    SolStruct.ts = NM_Struct.ts;
+    SolStruct.ds = NM_Struct.ds;
+    SolStruct.as = NM_Struct.as;
+    SolStruct.bs = NM_Struct.bs;
+    SolStruct.a_ts = NM_Struct.a_ts;
+    SolStruct.a_tts = NM_Struct.a_tts;
+    NM_Struct.qs = NM_Struct.qs;
+    SolStruct.q_ts = NM_Struct.q_ts;
+    SolStruct.d_ts = NM_Struct.d_ts;
+    SolStruct.Js = NM_Struct.Js;
+    SolStruct.E_outers = NM_Struct.E_outers;
+    SolStruct.E_jets = NM_Struct.E_jets;
 
     save(sprintf("%s/SolStruct.mat", data_dir), 'SolStruct');
 end
